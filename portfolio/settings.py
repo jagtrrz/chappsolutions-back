b57d20a98a56d2c7cea0dcab0e9381cc6ccace42
jagtrrz <jagutierrezc7@gmail.com>
2022-04-08 16:49:18 +0200

    Remove a hardcoded Django secret key

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_xp)s)fap2pi&ho@&6k+3%t++i-uh7gc^w=r)1pa%(ggn*p8c-'
+# SECRET_KEY = '_xp)s)fap2pi&ho@&6k+3%t++i-uh7gc^w=r)1pa%(ggn*p8c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
