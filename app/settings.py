9649bd65bfa54de682cecae5e06b5704873d22f1
jagtrrz <jagutierrezc7@gmail.com>
2022-04-10 18:26:44 +0200

    forgot database settings

--- app/settings.py
+++ app/settings.py
@@ -92,15 +92,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL),
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=DATABASE_URL),
 }
 
 # Password validation
